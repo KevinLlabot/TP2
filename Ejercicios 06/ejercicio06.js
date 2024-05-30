e2d26c3bfb35014c6f7d09b952c769da55a06317
Kevin Llabot <kevinllabot@hotmail.com>
2024-05-30 01:06:45 -0300

    Se agrega ejercicio 06

---
+++ Ejercicios 06/ejercicio06.js
@@ -0,0 +1,42 @@
+    const nombre = $("nombre")
+    const apellido = $("apellido")
+    const edad = $("edad")
+    const altura = $("altura")
+    const correo = $("correo")
+
+    formulario.addEventListener("submit", (event) => {
+        event.preventDefault(); // Evita el envío del formulario para realizar la validación
+        
+        mensaje.style.color = "red";
+        
+        if (formulario["nombre"].value === "") {
+            mensaje.textContent = "Nombre vacio";
+        } else if (formulario["apellido"].value === "") {
+            mensaje.textContent = "Apellido vacio";
+        } else if (formulario["edad"].value === "") {
+            mensaje.textContent = "La edad esta vacia";
+        } else if (formulario["edad"].value < 0) {
+            mensaje.textContent = "La edad es negativa";
+        } else if (formulario["edad"].value < 18) {
+            mensaje.textContent = "No se puede enviar menor de edad";
+        } else if (formulario["altura"].value === "") {
+            mensaje.textContent = "La altura esta vacia";
+        } else if (formulario["altura"].value <= 0) {
+            mensaje.textContent = "La altura no puede ser negativa o '0'";
+        } else if (formulario["altura"].value > 230) {
+            mensaje.textContent = "La altura no puede ser mayor a 230 cm";
+        } else if (formulario["correo"].value === "") {
+            mensaje.textContent = "Correo vacio";
+        } else if (!formulario["correo"].value.includes("@")) {
+            mensaje.textContent = "Debe contener una '@' al menos...";
+        } else {
+            mensaje.textContent = "Todos los datos son correctos";
+            mensaje.style.color = "green";
+        }
+    });
+    
+
+    function $(elemento) {
+      return document.getElementById(elemento);
+    }
+  
