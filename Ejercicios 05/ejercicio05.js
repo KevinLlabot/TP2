195a436f8e29f44ebdc363b4bc6250c33b1d1662
Kevin Llabot <kevinllabot@hotmail.com>
2024-05-30 20:04:14 -0300

    fin

--- Ejercicios 05/ejercicio05.js
+++ Ejercicios 05/ejercicio05.js
@@ -1,8 +1,8 @@
 function calcularArea(){   
 
-let a = document.getElementById("alturaA").value;
-let b = document.getElementById("baseB").value;
-let c = document.getElementById("alturaC").value;
+let a = parseInt(document.getElementById("alturaA").value);
+let b = parseInt(document.getElementById("baseB").value);
+let c = parseInt(document.getElementById("alturaC").value);
 
 if (isNaN(a) || isNaN(b) || isNaN(c) || a <= 0 || b <= 0 || c <= 0 || c >= a){
     alert("Por favor ingrese numeros mayor a '0' y 'A' tiene que ser mayor que 'C'")
